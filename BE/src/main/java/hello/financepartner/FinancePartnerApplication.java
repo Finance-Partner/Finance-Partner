3b293f0c7bad9bd271bddf0b6a6ba00743922f64
Hyunuk Shin <shu914@naver.com>
2024-02-05 07:45:57 +0900

    초기 설정

---
+++ BE/src/main/java/hello/financepartner/FinancePartnerApplication.java
@@ -0,0 +1,13 @@
+package hello.financepartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancePartnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancePartnerApplication.class, args);
+    }
+
+}
